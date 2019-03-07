c4db6268da817c91c9b76c01d035cf0fb557b723
JP Arimaa <juhapekka.arimaa@gmail.com>
2019-03-07 20:50:57 +0200

    Update tree materials, make trees grow

--- exile/Source/exile/Tree.cpp
+++ exile/Source/exile/Tree.cpp
@@ -1,5 +1,12 @@
 #include "Tree.h"
 
+namespace
+{
+const float c_maxYield = 100.0f;
+const float c_timeToMaxYield = 30.0f;
+const float c_yieldPerSecond = c_maxYield / c_timeToMaxYield;
+}
+
 ATree::ATree()
 {
 	PrimaryActorTick.bCanEverTick = true;
@@ -13,6 +20,13 @@ void ATree::BeginPlay()
 void ATree::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	if (!m_isFullGrown)
+	{
+		FVector scale(0.5f + FMath::Lerp(0.0f, 0.5f, m_yield / c_maxYield));
+		SetActorScale3D(scale);
+		m_yield += DeltaTime * c_yieldPerSecond;
+		m_isFullGrown = m_yield > c_maxYield;
+	}
 }
 
 bool ATree::cut(uint16 damage, uint16& yield)
@@ -20,10 +34,20 @@ bool ATree::cut(uint16 damage, uint16& yield)
 	m_health -= damage;
 	if (m_health <= 0)
 	{
-		yield = m_yield;
+		yield = getYield();
 		Destroy();
 		return true;
 	}
 	yield = 0;
 	return false;
+}
+
+uint16_t ATree::getYield()
+{
+	return static_cast<uint16_t>(m_yield);
+}
+
+bool ATree::isFullGrown()
+{
+	return m_isFullGrown;
 }
--- exile/Source/exile/Tree.h
+++ exile/Source/exile/Tree.h
@@ -14,12 +14,16 @@ public:
 	virtual void Tick(float DeltaTime) override;
 	bool cut(uint16 damage, uint16& yield);
 
+	uint16_t getYield();
+	bool isFullGrown();
+
 	UPROPERTY(EditAnywhere)
 		uint16 m_health = 100;
 
-	UPROPERTY(EditAnywhere)
-		uint16 m_yield = 100;
-
 protected:
 	virtual void BeginPlay() override;
+
+private:
+	float m_yield = 0;
+	bool m_isFullGrown = false;
 };
